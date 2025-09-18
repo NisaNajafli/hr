3520a953bbbafb40a189ca2d1c771cc9b4f9932f
NisaNajafli <ncfliniss@gmail.com>
2025-09-18 20:34:50 +0400

    update

---
+++ main.js
@@ -0,0 +1,14 @@
+window.addEventListener("scroll", function () {
+  const header = document.querySelector("header");
+  if (window.scrollY > 0) {
+    header.classList.add("scrolled");
+  } else {
+    header.classList.remove("scrolled");
+  }
+});
+
+var mainDiv = document.getElementById('main-button');
+mainDiv.addEventListener('click', function(){
+  this.children.item(0).classList.toggle('fa-times');
+  this.classList.toggle('open');
+});
