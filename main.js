f7bd5c5743eac5fb02af3ae7b9830d75ecaedf75
NisaNajafli <ncfliniss@gmail.com>
2025-09-19 17:36:36 +0400

    new

--- main.js
+++ main.js
@@ -6,6 +6,14 @@ window.addEventListener("scroll", function () {
     header.classList.remove("scrolled");
   }
 });
+window.addEventListener("scroll", () => {
+  const header = document.getElementById("site-header");
+  if (window.scrollY > 150) {        
+    header.classList.add("scrolled");
+  } else {
+    header.classList.remove("scrolled");
+  }
+});
 
 var mainDiv = document.getElementById('main-button');
 mainDiv.addEventListener('click', function(){
