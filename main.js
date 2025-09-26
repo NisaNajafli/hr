fabeacd2bc048d642cc48f2e065b2968da4e5238
NisaNajafli <ncfliniss@gmail.com>
2025-09-26 17:48:39 +0400

    new

--- main.js
+++ main.js
@@ -98,4 +98,17 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 document.getElementById('year').textContent = new Date().getFullYear();
 
-
+document.addEventListener('DOMContentLoaded', function () {
+  new Swiper('.mySwiper', {
+  loop: true, 
+  slidesPerView: 6,
+  spaceBetween: 30,
+  autoplay: { delay: 3000, disableOnInteraction: false },
+  pagination: { el: '.swiper-pagination', clickable: true },
+  navigation: { nextEl: '.swiper-button-next', prevEl: '.swiper-button-prev' },
+  breakpoints: {
+    768: { slidesPerView: 1 },
+    1200: { slidesPerView: 5}
+  }
+});
+});
