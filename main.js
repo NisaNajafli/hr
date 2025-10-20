94bfda10154566f03b5c763deab80558acfa9ac8
Nisa Najafli <ncfliniss@gmail.com>
2025-10-21 00:06:53 +0400

    up

--- main.js
+++ main.js
@@ -144,7 +144,8 @@ document.addEventListener('DOMContentLoaded', function () {
   pagination: { el: '.swiper-pagination', clickable: true },
   navigation: { nextEl: '.swiper-button-next', prevEl: '.swiper-button-prev' },
   breakpoints: {
-    768: { slidesPerView: 1 },
+    0: {slidesPerView: 2},
+    768: { slidesPerView: 3 },
     1200: { slidesPerView: 5}
   }
 });
